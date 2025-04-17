8e478617b9ff4b511ccbf27eb8ba1d2d05f49788
Kester <kestertan040@gmail.com>
2025-04-17 16:08:16 -0400

    hardcode repeat

--- transfer.py
+++ transfer.py
@@ -384,7 +384,7 @@ def create_csvs_from_node_data_array(
                     else:
                         matching_indices = [i for i, col in enumerate(df.columns) if col_name in col]
                         df_columns = df.iloc[:, matching_indices]
-                        df_columns = df_columns.loc[df_columns.index.repeat(len(matching_indices))].reset_index(drop=True)
+                        df_columns = df_columns.loc[df_columns.index.repeat(3)].reset_index(drop=True)
                     new_df[col_name] = pd.DataFrame(df_columns.values.flatten(), columns=[col_name])
                 else:
                     matching_indices = [i for i, col in enumerate(df.columns) if col_name == col]
